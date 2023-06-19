a31d6683f7300cb1b995688b76e0281bb21a74f9
DevBrianAcostaBenitez <brianacostabenitez@gmail.com>
2023-06-19 19:17:03 +0100

    created list that takes data from form via father-son communication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
+import { PeopleListComponent } from './people-list/people-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponent
+    FormComponent,
+    PeopleListComponent
   ],
   imports: [
     BrowserModule,
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,20 +1,29 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { PeopleListComponent } from '../people-list/people-list.component';
+
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  styleUrls: ['./form.component.css'],
+  
 })
+
 export class FormComponent {
-  peopleForm = new FormGroup({
-    id: new FormControl(''),
-    name: new FormControl(''),
-    lastName: new FormControl(''),
-    position: new FormControl(''),
-    salary: new FormControl(''),
-  });
+  public peopleForm: FormGroup;
+  public dataList: any[] = [];
+  constructor() {
+    this.peopleForm = new FormGroup({
+      id: new FormControl(''),
+      name: new FormControl(''),
+      lastName: new FormControl(''),
+      position: new FormControl(''),
+      salary: new FormControl(''),
+    });
+  }
   submitPerson() {
-    
-   
+    const formData = this.peopleForm.value;
+    this.dataList.push(formData);
+    this.peopleForm.reset();
   }
 }
